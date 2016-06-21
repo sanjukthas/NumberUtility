d215e2a1a304604a7dc686136d10dff3b55e5c93
sanjukthas <sanjuktha23@gmail.com>
2016-06-21 13:12:41 -0400

    hashmap

--- NumberUtility/src/NumberUility/Numbertest.java
+++ NumberUtility/src/NumberUility/Numbertest.java
@@ -6,12 +6,12 @@ import NumberUility.NumberUtility;
 public class Numbertest {
 
 	public static void main(String[] args) {
-		System.out.println(NumberUtility.formatAsPercentage(67.2));
-		System.out.println(NumberUtility.formatInCcurrency(67.2));
-		System.out.println(NumberUtility.formatwithDecimalPlaces(67.25555558, 5));
-		System.out.println(NumberUtility.random(5, 50));
-		System.out.println(NumberUtility.max(50.058, 50));
-		System.out.println(NumberUtility.min(50.058, 50));
+		System.out.println(NumberUtility.formatAsPercentage(35.6));
+		System.out.println(NumberUtility.formatInCcurrency(35.6));
+		System.out.println(NumberUtility.formatwithDecimalPlaces(55.120536, 5));
+		System.out.println(NumberUtility.random(6, 60));
+		System.out.println(NumberUtility.max(60.059, 60));
+		System.out.println(NumberUtility.min(60.059, 60));
 
 	}
 
